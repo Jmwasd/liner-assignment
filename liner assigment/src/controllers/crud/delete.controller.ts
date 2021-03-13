ce753837f10616c68d11d019a63c62418a2413fe
Jmwasd <66477332+Jmwasd@users.noreply.github.com>
2021-03-13 12:28:33 +0900

    delete, resign 기능구현 완료

--- liner assigment/src/controllers/crud/delete.controller.ts
+++ liner assigment/src/controllers/crud/delete.controller.ts
@@ -1,7 +1,37 @@
-import {expressFn} from '../../types/common.types';
+import { Op } from 'sequelize';
+import DB from '../../database';
+import {expressFn, highlightType} from '../../types/common.types';
 
 const deletes: expressFn = async(req, res)=>{
-    
+    const {highlights,pages} = DB;
+    const {userId , highlightId} = req.body;
+
+    const findHighlightData:highlightType = await highlights.findOne({
+        where : {highlightId : highlightId}
+    })
+
+    await highlights.destroy({
+        where : {[Op.and]: [
+            {userId : userId},
+            {highlightId : highlightId}
+        ]}
+    }).then(()=>{
+        return highlights.findOne({
+            where : {[Op.and]: [
+                {userId : userId},
+                {pageId : findHighlightData.pageId}
+            ]}
+        })
+    }).then((result : highlightType | undefined)=>{
+        if(!result){
+            pages.destroy({
+                where : {id : findHighlightData.pageId}
+            })
+        }
+        res.send("200 OK");
+    })
+
+
 };
 
 export default deletes;
